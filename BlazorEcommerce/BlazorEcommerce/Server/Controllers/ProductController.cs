a9bbd269f9e03304b13e5468ee7a1d4dced6bff4
Akihira77 <tik.dikawahyu123@gmail.com>
2023-03-02 08:37:27 +0700

    Add project files.

---
+++ BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using BlazorEcommerce.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers;
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+	private readonly AppDbContext _db;
+
+	public ProductController(AppDbContext db)
+    {
+		_db = db;
+	}
+
+	[HttpGet("Get-all-products")]
+	public async Task<ActionResult<List<Product>>> GetAllProducts()
+	{
+		var products = await _db.Products.ToListAsync();
+		return Ok(products);
+	}
+}
